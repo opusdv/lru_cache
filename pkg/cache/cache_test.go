4495edb9a6521e0d0752fc1ff9f98260c6452917
Your Name <you@example.com>
2021-12-07 01:02:38 +0300

    add test,doc,examples

--- pkg/cache/cache_test.go
+++ pkg/cache/cache_test.go
@@ -49,3 +49,42 @@ func TestCacheAdd(t *testing.T) {
 	}
 
 }
+
+func TestCacheRemove(t *testing.T) {
+	req := require.New(t)
+	cache := NewLRUCache(5)
+	tests := map[string]struct {
+		firstVal  string
+		secondVal string
+		want      bool
+	}{
+		"simple": {firstVal: "TestKey1", secondVal: "TestValue1", want: true},
+	}
+	for name, testCase := range tests {
+		cache.Add(testCase.firstVal, testCase.secondVal)
+		t.Run(name, func(t *testing.T) {
+			res := cache.Remove(testCase.firstVal)
+			req.Equal(res, testCase.want)
+		})
+	}
+}
+
+func TestCacheGet(t *testing.T) {
+	req := require.New(t)
+	cache := NewLRUCache(5)
+	tests := map[string]struct {
+		firstVal  string
+		secondVal string
+		want      bool
+	}{
+		"simple": {firstVal: "TestKey1", secondVal: "TestValue1", want: true},
+	}
+	for name, testCase := range tests {
+		cache.Add(testCase.firstVal, testCase.secondVal)
+		t.Run(name, func(t *testing.T) {
+			res, ok := cache.Get(testCase.firstVal)
+			req.NotEqual("", res)
+			req.Equal(ok, testCase.want)
+		})
+	}
+}
